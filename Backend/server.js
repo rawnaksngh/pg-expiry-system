a8a319b00c5b8ba3f9442303b08f9f9d4760a89e
Rounak Singh <ROUNAKSIGH@gmail.com>
2026-05-12 15:09:33 +0530

    render port fix

--- Backend/server.js
+++ Backend/server.js
@@ -99,9 +99,13 @@ app.get("/", (req, res) => {
 
 });
 // SERVER START
-app.listen(5000, () => {
+const PORT =
+  process.env.PORT || 5000;
+
+app.listen(PORT, () => {
 
   console.log(
-    "Server running on port 5000"
+    `Server running on port ${PORT}`
   );
+
 });
